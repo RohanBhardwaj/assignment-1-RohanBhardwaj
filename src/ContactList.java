d4f5b54df170a63b336df829c5e9388d8ffac3a2
Rohan Bhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2020-02-22 18:22:46 +0530

    created person class

--- src/ContactList.java
+++ src/ContactList.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class ContactList {
     public static void main(String[] args) {
-        System.out.println("Welcome to DBC's Contact List App");
+        System.out.println("Welcome to Rohan Bhardwaj Contact List App");
         System.out.println("Press 1 to add a new contact");
         System.out.println("Press 2 to view all contacts");
         System.out.println("Press 3 to search for a contact");
         System.out.println("Press 4 to delete a contact");
         System.out.println("Press 5 to exit program");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    private String FName;
+    private String LName;
+    private long contact;
+    private String email;
+
+    public Person(String FName, String LName, long contact, String email) {
+        this.FName = FName;
+        this.LName = LName;
+        this.contact = contact;
+        this.email = email;
+    }
+
+
+}
